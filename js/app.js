1c7265f77fca7e8823ab97ca045b0391c5adb962
EmmanuelAbajo <temiabajo@gmail.com>
2021-03-29 22:59:05 +0100

    removed unnecessary js code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 const imgArr = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
 
-let divImage = document.getElementById("images");
 let myImage = document.getElementById("myImg");
-let main = document.getElementById("mainPage");
-
-let index = 0
-let mIndex = imgArr.length - 1
 
 function displayImageOnClick(src_) {
     myImage.src = src_
@@ -34,65 +29,4 @@ function moveBack() {
     }
 }
 
-function display(index) {
-    main.innerHTML = "<img src='" + imgArr[index] + "'>"
-}
-
-function one() {
-    display(0)
-}
-
-function two() {
-    display(1)
-}
-
-function three() {
-    display(2)
-}
-
-function four() {
-    display(3)
-}
-
-
-
-function forward() {
-    if (display(index) == 0) {
-        index = 0
-    } else if (display(index) == 1) {
-        index = 1
-    } else if (display(index) == 2) {
-        index = 2
-    } else if (display(index) == 3) {
-        index = 3
-    }
-    index++
-    if (index < imgArr.length) {
-        main.innerHTML = "<img src='" + imgArr[index] + "'height = 200px width=200px>"
-    } else if (index == imgArr.length) {
-        index = 0
-        main.innerHTML = "<img src='" + imgArr[index] + "'height = 200px width=200px>"
-    }
-
-}
-
-function back() {
-    if (display(index) == 0) {
-        index = 0
-    } else if (display(index) == 1) {
-        index = 1
-    } else if (display(index) == 2) {
-        index = 2
-    } else if (display(index) == 3) {
-        index = 3
-    }
-    index--
-    if (index >= 0) {
-        main.innerHTML = "<img src='" + imgArr[index] + "'height = 200px width=200px>"
-    }
-    if (index < 0) {
-        index = imgArr.length - 1
-        main.innerHTML = "<img src='" + imgArr[index] + "'height = 200px width=200px>"
-    }
-}
 
